4bc3c490673c9b52c3a55e62bf2c606b8df5dd6a
Juliet <m@juli.ee>
2025-04-21 14:16:30 +0200

    make constant private

--- tid/b32.go
+++ tid/b32.go
@@ -2,7 +2,7 @@ package tid
 
 import "strings"
 
-const B32Sorted = "234567abcdefghijklmnopqrstuvwxyz"
+const b32Sorted = "234567abcdefghijklmnopqrstuvwxyz"
 
 func b32Encode(i int64) string {
 	s := ""
@@ -10,7 +10,7 @@ func b32Encode(i int64) string {
 	for i != 0 {
 		c := i % 32
 		i = i / 32
-		s = B32Sorted[c:c+1] + s
+		s = b32Sorted[c:c+1] + s
 	}
 
 	return s
@@ -20,7 +20,7 @@ func b32Decode(s string) int64 {
 	var i int64 = 0
 
 	for _, c := range s {
-		i = i*32 + int64(strings.IndexRune(B32Sorted, c))
+		i = i*32 + int64(strings.IndexRune(b32Sorted, c))
 	}
 
 	return i
